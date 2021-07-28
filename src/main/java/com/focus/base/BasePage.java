44ddc8ba44419241dc02a69ab97cdcb32c4a79fa
reyes-manny <ingmareys@gmail.com>
2021-07-27 19:46:02 -0600

    test

--- src/main/java/com/focus/base/BasePage.java
+++ src/main/java/com/focus/base/BasePage.java
@@ -32,7 +32,7 @@ public class BasePage extends Page {
 		String browserName = prop.getProperty("browser");
 		if(browserName.equals("chrome")) {
 			optionsManager = new OptionsManager(prop);
-			System.setProperty("webdriver.chrome.driver","src/test/java/chrome/chromedriver");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver");
 			driver = new ChromeDriver(optionsManager.getChromeOptions());
 		}
 		/*else if(browserName.equals("firefox")) {
